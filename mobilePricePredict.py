bc14a86c4f94b545e8f7d2a7104a027399fdcf74
ShridharSChini <34676240+ShridharSChini@users.noreply.github.com>
2023-02-25 11:24:46 +0530

    Initial commit

---
+++ mobilePricePredict.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import pandas as pd
+from bs4 import BeautifulSoup
+
+from tabulate import tabulate
+
+pd.set_option('display.max_colwidth', None)
+
+soup = BeautifulSoup(requests.get("https://www.smartprix.com/ui/api/page-info?k=1SYMJDYwYlHJG5M4LKl_IzKE2JLH9GF6YiY52L2YwSUiYLYwnsttovtrnrrmmiYKLYwnsttovtqosvvpU").content)
+res = json.loads(soup.text)
+res = (res["item"]["searchResults"]["nodes"])
+print("\n\n\\n res ********\n")
+
+data = {"Name":[],"Store":[],"Best Price":[]}
+
+for product in res:
+    if product["stock"] != "UPCOMING":
+        data["Name"].append(product["name"])
+        data["Store"].append(product["bestStore"]["store"]["slug"])
+        data["Best Price"].append(product["price"])
+
+        data["Name"].append(" ")
+        data["Store"].append(" ")
+        data["Best Price"].append(" ")
+
+df = pd.DataFrame(data)
+print(tabulate(df, showindex=False, headers=df.columns))
